3e7544de3c66a40579d9551a07a88442c95bf0d1
Daniel Hernandez <daniher02@gmail.com>
2025-01-12 14:51:18 -0300

    Actualizar configuración de Astro: modificar la importación del manifiesto y cambiar middlewareSecret.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
+// import tailwind from "@astrojs/tailwind";
+// import compress from "astro-compress";
+// import icon from "astro-icon";
+// import { defineConfig } from 'astro/config';
+
+// import vercel from "@astrojs/vercel/serverless";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [tailwind(), icon(), compress()],
+//   output: "server",
+//   adapter: vercel()
+// });
+
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), compress()],
+  integrations: [tailwind(), icon()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
