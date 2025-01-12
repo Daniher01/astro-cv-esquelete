c9f0e8211a9b427feaf8dd676898517d504ecf15
Daniel Hernandez <daniher02@gmail.com>
2025-01-12 15:24:08 -0300

    Eliminar configuraciones obsoletas en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,28 +11,3 @@ export default defineConfig({
   output: "static"
   //adapter: vercel()
 });
-
-// import tailwind from "@astrojs/tailwind";
-// import { defineConfig } from "astro/config";
-// import vercel from "@astrojs/vercel/serverless";
-// import icon from "astro-icon";
-
-// export default defineConfig({
-//   integrations: [tailwind(), icon()],
-//   output: "server",
-//   adapter: vercel(),
-// });
-
-
-// import { defineConfig } from 'astro/config';
-// import node from '@astrojs/node';
-// import icon from "astro-icon";
-// import tailwind from "@astrojs/tailwind";
-
-// export default defineConfig({
-//   integrations: [tailwind(), icon()],
-//   output: 'static',  // Puedes usar "server" o "hybrid" dependiendo de tus necesidades
-//   adapter: node({
-//     mode: 'standalone',
-//   }),
-// });
